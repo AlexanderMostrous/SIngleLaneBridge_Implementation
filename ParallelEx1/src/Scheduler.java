9b96348f6719874bad76595be9644b884dedc600
alexandros <alexandros@Alexander-Desktop>
2019-07-03 11:35:50 +0300

    Modelling fixed. Interface not needed.

--- ParallelEx1/src/Scheduler.java
+++ ParallelEx1/src/Scheduler.java
@@ -1,6 +1,8 @@
 
-public abstract class Scheduler implements Scheduling {
+public abstract class Scheduler{
 
-	
+	public abstract void crossBridge(Car c);
+	public abstract void enterBridge(Car c);
+	public abstract void exitBridge(Car c);
 	
 }
--- ParallelEx1/src/Scheduling.java
+++
@@ -1,7 +0,0 @@
-
-public interface Scheduling {
-
-	public void crossBridge(Car c);
-	public void enterBridge(Car c);
-	public void exitBridge(Car c);
-}
