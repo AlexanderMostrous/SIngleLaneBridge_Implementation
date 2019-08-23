1cb5dbfb551b1ec18d714fc5e02f22ae2b1cef87
alexandros <alexandros@Alexander-Desktop>
2019-08-23 22:54:04 +0300

    SafeRoundRobbinShcheduler progress.

--- ParallelEx1/src/SafeRoundRobbinScheduler.java
+++ ParallelEx1/src/SafeRoundRobbinScheduler.java
@@ -2,11 +2,9 @@ import java.util.concurrent.Semaphore;
 
 public class SafeRoundRobbinScheduler extends Scheduler{
 
-	private boolean bridgeIsEmpty;
-	private int nextRedToPass, nextBlueToPass, nextColour;
+	private int nextColour;
 	
-	private Semaphore waiting = new Semaphore(1);
-	private Semaphore leaving = new Semaphore(0);
+	private Semaphore waitingBlue = new Semaphore(1), waitingRed = new Semaphore(1);
 
 	/*
 	 * To perasma einai asfales xwris sygkrouseis kai ta aftokinhta pernane 
@@ -17,9 +15,7 @@ public class SafeRoundRobbinScheduler extends Scheduler{
 	{
 		super(time, cars);
 		
-		bridgeIsEmpty = true;
-		nextBlueToPass = 1;
-		nextRedToPass = 1;
+		
 		nextColour = 1;
 	}
 
@@ -27,86 +23,14 @@ public class SafeRoundRobbinScheduler extends Scheduler{
 	{
 		enterBridge(c);
 	}
-
-	/*
-	 * 
-	 * To amaksi (nhma) pou 8elei na mpei sth gefyra,
-	 * 8a tou apagoreftei h eisodos (wait) an h gefyra den einai adeia,
-	 * an to xrwma tou den einai to swsto (afto tou opoiou einai h seira)
-	 * h an t idio den einai to prwto amaksi ths seiras tou.
-	 */
 	public synchronized void enterBridge(Car c)
 	{
-		while ((!bridgeIsEmpty)||(nextColour!=c.getColour()))
-		{
-			//System.out.println("Failed because bridgeIsEmpty = "+bridgeIsEmpty+" and c.getNum() = "+c.getNum()+" while nextToPass = "+nextToPass);
-			try 
-			{
-				Thread.sleep(10);
-				wait();
-			} 
-			catch (InterruptedException e) 
-			{
-				e.printStackTrace();
-			}
-		}
-		
-		/*
-		 * To amaksi (nhma) pou 8elei na mpei sth gefyra, 8a tou apagoreftei
-		 * h eisodos (wait) an t idio den einai to prwto amaksi ths seiras tou (2os elegxos).
-		 */
-		
-		int nextToPass;
-		if(c.getColour()==1)
-			nextToPass = nextBlueToPass;
-		else
-			nextToPass = nextRedToPass;
-		
-		while (c.getNum()!=nextToPass)
-		{
-			try 
-			{
-				wait();
-			} 
-			catch (InterruptedException e) 
-			{
-				e.printStackTrace();
-			}
-		}
-
-		bridgeIsEmpty = false;
 		c.setPassing(System.currentTimeMillis());
-		
-		try 
-		{
-			Thread.sleep(Main.bridgeDelay);
-		} 
-		catch (InterruptedException e) 
-		{
-			e.printStackTrace();
-		}
-		
-		exitBridge(c);
 	}
 
-	public synchronized void exitBridge(Car c)
-	{
+	public synchronized void exitBridge(Car c) {
 		c.setPassed(System.currentTimeMillis());
-		
-		bridgeIsEmpty = true;
-		
-		if(c.getColour()==1)
-		{
-			nextBlueToPass++;
-			nextColour = 2;
-		}
-		else
-		{
-			nextRedToPass++;
-			nextColour = 1;
-		}
-		
-		notifyAll();
+		c.setFinishedPassing(true);
 	}
 
 }
